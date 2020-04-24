4bfafe7254267505b642aaec42d6e63a790d2f24
hassan kiwan <hassanhitleap1@gmail.com>
2020-04-24 21:30:13 +0300

    first commit

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+      <h1>this id is {{$route.params.id}}</h1>
+  </div>
+</template>
+<script>
+  export default {
+      validate ({ params }) {
+          // Must be a number
+          return /^\d+$/.test(params.id)
+      }
+  }
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+      <div class="row">
+        <input v-model="id" class="form-control" type="text" />
+        <button class="btn btn-success" @click="loder" @keypress="loder">loading</button>
+      </div>
+  </div>
+</template>
+<script>
+  export default {
+      data:{
+          id:''
+      },methods:{
+          loder(){
+              this.$router.push(`/users/${this.id}`);
+          }
+      }
+  }
+</script>
